8419923c3177f6dd4f046df755eeafeb81f56f08
saltonmassally <salton.massally@gmail.com>
2019-10-17 20:10:31 +0000

    add cognito utils

---
+++ mikashbokspy/users.py
@@ -0,0 +1,28 @@
+import boto3
+import os
+import logging
+
+logger = logging.getLogger()
+cognito_client = boto3.client('cognito-idp')
+
+
+def get_user(user_id):
+    responses = cognito_client.list_users(
+        UserPoolId=os.getenv("COGNITO_USERPOOL_ID"),
+        AttributesToGet=None,
+        Limit=1,
+        Filter="sub = \"" + user_id + "\""
+    )
+    return responses["Users"][0] if responses["Users"] else None
+
+
+def get_user_attribute(user_id, attribute):
+    user = get_user(user_id)
+    if not user:
+        return None
+
+    for attribute in user['Attributes']:
+        if attribute["Name"] == attribute:
+            return attribute["Value"]
+    logger.error("Could not determine " + attribute + " for user " + user_id)
+    return None
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ REQUIREMENTS = [str(ir.req) for ir in parse_requirements('requirements.txt', ses
 
 setup(
     name='mikashbokspy',
-    version='0.0.3',
+    version='0.0.4',
     description='Common python utilities for mikashboks microservices in a aws lambda environment',
     author='Salton Massally',
     author_email='smassally@idtlabs.xyz',
